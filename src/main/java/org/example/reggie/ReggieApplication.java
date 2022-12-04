0f5801c4aca5cce2332b7f6bdae3a74eff48491e
WebbyBubble <714780629@qq.com>
2022-12-04 12:29:00 +0800

    使用Spring Cache缓存套餐数据

--- src/main/java/org/example/reggie/ReggieApplication.java
+++ src/main/java/org/example/reggie/ReggieApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /*
@@ -15,6 +16,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @ServletComponentScan
 @EnableTransactionManagement
+@EnableCaching//开启spring chche注解方式的缓存功能
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
