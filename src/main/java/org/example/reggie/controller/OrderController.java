6e0a57bc42bcc1e4572899712373a3be03d65ec6
WebbyBubble <714780629@qq.com>
2022-12-11 23:16:01 +0800

    后台订单明细后端代码完成

--- src/main/java/org/example/reggie/controller/OrderController.java
+++ src/main/java/org/example/reggie/controller/OrderController.java
@@ -1,14 +1,13 @@
 package org.example.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
 import org.example.reggie.common.R;
 import org.example.reggie.entity.Orders;
 import org.example.reggie.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -22,4 +21,75 @@ public class OrderController {
         orderService.submit(orders);
         return R.success("下单成功");
     }
+/*    *//**
+     * 页面显示
+     * @param page
+     * @param pageSize
+     * @return
+     *//*
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize){
+        //页面构造器
+        Page<Orders> pageInfo = new Page<>(page, pageSize);
+
+        //查询所有orders表信息
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByDesc(Orders::getOrderTime);
+        orderService.page(pageInfo, queryWrapper);
+
+        return R.success(pageInfo);
+    }*/
+    /**
+     * 页面显示——输入框查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, Long number, String beginTime, String endTime){
+        //log.info("beginTime:{}",beginTime);
+        //log.info("endTime:{}",endTime);
+        //页面构造器
+        Page<Orders> pageInfo = new Page<>(page, pageSize);
+
+        //查询所有orders表信息
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        //查询name
+        if (number != null){
+            queryWrapper.like(Orders::getNumber, number);
+        }
+        //查询beginTime 大于等于这个时间
+        if (beginTime != null){
+            queryWrapper.ge(Orders::getOrderTime, beginTime);
+        }
+        //查询endTime 小于等于这个时间
+        if (endTime != null){
+            queryWrapper.le(Orders::getOrderTime, endTime);
+        }
+        queryWrapper.orderByDesc(Orders::getOrderTime);
+        orderService.page(pageInfo, queryWrapper);
+
+        return R.success(pageInfo);
+    }
+    /**
+     * 订单状态修改——管理端
+     * @param orders
+     * @return
+     */
+    @PutMapping
+    public R<String> order(@RequestBody Orders orders){
+        //log.info("orders:{}", orders);
+        Orders order = orderService.getById(orders.getId());
+        if (order.getStatus() == 2){
+            orders.setStatus(3);
+            orderService.updateById(orders);
+            return R.success("订单派送成功");
+        }else {
+            orders.setStatus(4);
+            orderService.updateById(orders);
+            return R.success("订单已完成");
+        }
+    }
+
+
 }
