4f3e3250aa6e9effd3a32331179133fd213bb1bb
WebbyBubble <714780629@qq.com>
2022-12-11 23:28:32 +0800

    用户退出登录功能

--- src/main/java/org/example/reggie/controller/UserController.java
+++ src/main/java/org/example/reggie/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -96,4 +97,16 @@ public class UserController {
         }
         return R.error("登录失败");
     }
+    /**
+     * 用户退出
+     * @param request
+     * @return
+     */
+    @PostMapping("/loginout")
+    public R<String> logout(HttpServletRequest request){
+        //清理Session中保存的当前登陆用户的id
+        request.getSession().removeAttribute("user");
+        return R.success("退出成功");
+    }
+
 }
